a659bcd048807f7d41f178eb6745839384cca992
Chris <christofferjcm@gmail.com>
2024-02-07 15:42:05 +0100

    footer style

--- Web/src/components/Footer.jsx
+++ Web/src/components/Footer.jsx
@@ -1,62 +1,36 @@
 import "./Footer.css";
 
-
-
 function Footer() {
   return (
     <footer className="foots">
-      
+      <div className="footers">
+        <p>
+          Nackatronik Huvudkontor <br />
+          Tomteboda vägen 3A Sverige <br />
+          E-post: info@nackatronik.com <br />
+          Telefon: (+46) 115-780
+        </p>
 
-                   <div className="footers">
-                    <p>
-                    Nackatronik Huvudkontor
-                    Tomteboda vägen 3A
-                    Sverige
-                    E-post: info@nackatronik.com
-                    Telefon: (+46) 115-780
+        <p>
+          Kundservice Vanliga frågor <br />
+          Returpolicy Fraktinformation <br />
+          Villkor och bestämmelser <br />
+          Integritetspolicy
+        </p>
 
-                    </p>
-                    
-                    <p>
-                    Kundservice
-                    Vanliga frågor
-                    Returpolicy
-                    Fraktinformation
-                    Villkor och bestämmelser
-                    Integritetspolicy
-                    </p>
+        <p>
+          Håll Kontakten Följ oss på sociala medier för uppdateringar och
+          erbjudanden:
+          <br />
+          Facebook | Twitter | Instagram
+        </p>
+      </div>
 
-
-
-                    <p>
-                    Håll Kontakten
-                    Följ oss på sociala medier för uppdateringar och erbjudanden:
-                    Facebook | Twitter | Instagram
-                    </p>
-                    
-                  </div>
-
-                <div className="copyright">
-
-                 ©2024 Nackatronik. Alla rättigheter förbehållna.
-
-                </div>
-     
-              
-      
-      
+      <div className="copyright">
+        ©2024 Nackatronik. Alla rättigheter förbehållna.
+      </div>
     </footer>
   );
 }
 
 export default Footer;
-                
-                
-            
-                
-
-                
-          
-
-
-
